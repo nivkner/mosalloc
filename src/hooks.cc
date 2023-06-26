c669fd10094be5989388d1bcf20b6d2fbde8e56c
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-06-26 14:18:13 +0300

    Revert "change the level when stack unwinding"
    This reverts commit 51e5907f9a257ee3fdc0154a79889ca0d2bb6c32.

--- src/hooks.cc
+++ src/hooks.cc
@@ -192,7 +192,6 @@ static inline void mini_stack_unwind(int output_fd) {
 		if (bytes_written >= MAX_BYTES) {
 			write_all(output_fd, ptr_str_buff, bytes_written);
 		}
-		level += 1;
 	}
 
 	write_all(output_fd, ptr_str_buff, bytes_written);
